1ba3878fc75a6aa96c670182590197d67a3dca5c
Alessandro Rios <drios28@outlook.es>
2025-12-04 14:41:36 -0500

    chore: update ESLint configuration and package dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,35 +1,17 @@
-import { FlatCompat } from '@eslint/eslintrc'
-import { dirname } from 'path'
-import { fileURLToPath } from 'url'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname
-})
-
-const eslintConfig = [
-  ...compat.config({
-    extends: ['next/core-web-vitals', 'next/typescript'],
-    rules: {
-      'react/prop-types': 'off',
-      'react/react-in-jsx-scope': 'off',
-      '@typescript-eslint/no-floating-promises': 'off',
-      '@typescript-eslint/explicit-function-return-type': 'off',
-      '@typescript—eslint/no—misused—promises': 'off',
-      '@typescript-eslint/no-unsafe-argument': 'off',
-      '@typescript-eslint/no-misused-promises': 'off',
-      '@typescript-eslint/strict-boolean-expressions': 'off',
-      'quotes': [
-        'error',
-        'single'
-      ],
-      'multiline-ternary': 'off',
-      'camelcase': 'off'
-    },
-    ignorePatterns: ['src/core/components/ui/']
-  }),
-]
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+    'src/core/components/ui/**',
+  ]),
+])
 
 export default eslintConfig
